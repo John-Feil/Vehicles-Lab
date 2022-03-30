0086e54b6dc9c2e5f80c806e8b1fabd256f0180a
John-Feil <johnathancfeil@gmail.com>
2022-03-30 15:09:18 -0400

    new possible fix

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package morevehicles;
 
+import static morevehicles.Vehicle.*;
+
 public class Main {
     public static void main(String[] args) {
     Car car = new Car("Ford", 2, 3000.0d);
@@ -8,7 +10,7 @@ public class Main {
     MotorBike motorbike = new MotorBike("Harley",1,2350.0d);
     Vehicle[] vehicles = {car,truck,horse,motorbike};
     morevehicles.Vehicle.printCalcTotals(vehicles);
-    morevehicles.Vehicle.printAllVehicleNames(vehicles);
+    printAllVehicleNames(vehicles);
     horse.printExperience();
     motorbike.printExperience();
     Vehicle.printVehicleName(car);
